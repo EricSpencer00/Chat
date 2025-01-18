ef4d2fa78b9a937d15c66f6b4e6fdab647673641
ericspencer00 <espencer2@luc.edu>
2025-01-17 19:34:03 -0600

    Nonnull annotator

--- demo/src/main/java/com/example/demo/WebSocketConfig.java
+++ demo/src/main/java/com/example/demo/WebSocketConfig.java
@@ -6,18 +6,20 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import io.micrometer.common.lang.NonNull;
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
+    public void configureMessageBroker(@NonNull MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic"); // Enable topic-based messaging
         config.setApplicationDestinationPrefixes("/app"); // Prefix for sending messages
     }
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
+    public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
         registry.addEndpoint("/chat").withSockJS(); // WebSocket endpoint
     }
 }
