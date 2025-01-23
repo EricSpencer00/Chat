41a3dc7e143ae2791994fd1a751971914453151b
ericspencer00 <espencer2@luc.edu>
2025-01-23 10:47:10 -0600

    regex

--- server/src/main/java/com/example/demo/WebConfig.java
+++ server/src/main/java/com/example/demo/WebConfig.java
@@ -8,9 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/{spring:\\w+}").setViewName("forward:/index.html");
-        registry.addViewController("/**/{spring:\\w+}").setViewName("forward:/index.html");
-        registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
-                .setViewName("forward:/index.html");
+        // Catch-all route to forward to Angular's index.html
+        registry.addViewController("/{spring:[a-zA-Z0-9-_]+}").setViewName("forward:/index.html");
+        registry.addViewController("/**").setViewName("forward:/index.html");
     }
 }
