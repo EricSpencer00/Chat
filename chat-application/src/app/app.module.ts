02da5c75585f2dd2e154820710c7d6a391eb486e
ericspencer00 <espencer2@luc.edu>
2025-01-23 10:21:07 -0600

    env for oauth

--- chat-application/src/app/app.module.ts
+++ chat-application/src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module'; // App-level routing
 import { AppComponent } from './app.component'; // Root component
 import { ChatModule } from './chat/chat.module'; // Import the ChatModule
+import { ChatComponent } from './chat/chat.component'; // Import the ChatComponent
+import { ChatRoutingModule } from './chat/chat-routing.module'; // Import the ChatRoutingModule
 
 @NgModule({
   declarations: [
     AppComponent, // Declare the root component
+    ChatComponent, // Declare the ChatComponent
   ],
   imports: [
     BrowserModule, // Provides Angular's browser-specific functionality
     AppRoutingModule, // Routing for the root module
     ChatModule, // Import the feature module
+    ChatRoutingModule, // Routing for the
   ],
   providers: [],
   bootstrap: [AppComponent], // Bootstraps the root component
