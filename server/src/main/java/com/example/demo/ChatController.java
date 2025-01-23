9bcbf1e3f93d751fd652c558baa268791cd86d8e
ericspencer00 <espencer2@luc.edu>
2025-01-23 10:39:03 -0600

    routes

--- server/src/main/java/com/example/demo/ChatController.java
+++ server/src/main/java/com/example/demo/ChatController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ChatController {
@@ -11,5 +12,15 @@ public class ChatController {
     @SendTo("/topic/messages") // Broadcasts to this topic
     public Message handleMessage(Message message) {
         return message; // Simply returns the message to be broadcasted
+    }
+
+    @GetMapping("/") 
+    public String index() {
+        return "index"; // Returns the index.html file
+    }
+
+    @GetMapping("/error") 
+    public String error() {
+        return "error"; // Returns the error.html file
     }
 }
