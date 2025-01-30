d283349f4ae4a1d1933f7789b6d90906dd93af12
ericspencer00 <espencer2@luc.edu>
2025-01-30 17:58:32 -0600

    index route

--- chat/src/main/java/com/example/chat/ChatController.java
+++ chat/src/main/java/com/example/chat/ChatController.java
@@ -1,12 +1,19 @@
 package com.example.chat;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
+@RequestMapping("/")
 public class ChatController {
     @GetMapping("/chat")
     public String chat() {
         return "Hello, Chat!";
+    }
+
+    @GetMapping("/index") 
+    public String index() {
+        return "index.html";
     }
 }
