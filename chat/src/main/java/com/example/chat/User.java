d0b2ecf461013d18f3f1a19891cfb8186ebada91
ericspencer00 <espencer2@luc.edu>
2025-01-31 20:27:57 -0600

    pom dep.

---
+++ chat/src/main/java/com/example/chat/User.java
@@ -0,0 +1,36 @@
+package com.example.chat;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+    // Default constructor required by JPA
+    public User() {
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
